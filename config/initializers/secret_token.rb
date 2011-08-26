b5dcfea8bd0592a519343bab4335b5deebcfbcb8
Alex Valderrama <avald@cs.stanford.edu>
2011-08-26 17:04:50 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Plaza::Application.config.secret_token = '337cc3518804976c822141ef798e46b85203e62aa5909f57f7694b32a87b2a119b36e15f1ac293055b6ad074b62f52a7d36c00188e0588a2909bdf83e8b0e901'
